587c59e803206196284f0dd5cc54883ee1fbd4f8
RizwanAhmed3000 <rizz112khan@gmail.com>
2025-01-21 11:24:44 +0500

    secure auth routing

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,70 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import CustomBtn from "@/components/CustomBtn";
+import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  let deviceWidth = Dimensions.get("window").width;
+  let deviceHeight = Dimensions.get("window").height;
+  // console.log(deviceHeight / 2.5)
+  // console.log(deviceWidth)
+
+  const signInHandler = async () => {
+    console.log(form);
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome Back
+          </Text>
+        </View>
+        <View className="px-5 pb-3">
+          <InputField
+            label={"Email"}
+            placeholder={"Enter your Email"}
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label={"Password"}
+            placeholder={"Enter your Password"}
+            icon={icons.lock}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+            secureTextEntry={true}
+          />
+          <CustomBtn
+            title={"Sign In"}
+            className="mt-5"
+            onPress={signInHandler}
+          />
+
+          {/* oauth */}
+          <OAuth />
+
+          <Link
+            href={`/sign-up`}
+            className="mt-10 text-lg text-center text-general-200"
+          >
+            <Text>Don't have an account? </Text>
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
